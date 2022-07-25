9f01a16a28ada57059236c0c5969bb8bb7e91f87
Alejandro Vargas <alevargasguitar777@gmail.com>
2022-07-24 22:18:53 -0500

    home

--- main.py
+++ main.py
@@ -189,7 +189,22 @@ def update_a_user():
     tags=["Tweets"]
 )
 def home():
-    return {"Twitter API": "Working!"}
+    """
+    Thie path operation shows all tweets in the app
+
+    Parameters:
+        -
+
+    Returns a json list with all tweets in the app, with the folling keys:
+        - tweet_id: UUID
+        - content: str
+        - created_at: datetime
+        - updated_at: Optional[datetime]
+        - by: User
+    """
+    with open("tweets.json", "r", encoding="utf-8") as f:
+        results = json.loads(f.read())
+        return results
 
 ### Post a tweet
 @app.post(
