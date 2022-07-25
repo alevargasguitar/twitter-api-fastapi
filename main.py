e294d72f96d613f602fff9d4b2c52201497ee181
Alejandro Vargas <alevargasguitar777@gmail.com>
2022-07-24 22:01:11 -0500

    tweet_post

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ class Tweet(BaseModel):
         max_length=256
     )
     created_at: datetime = Field(default=datetime.now())
-    update_at: Optional[datetime] = Field(default=None)
+    updated_at: Optional[datetime] = Field(default=None)
     by: User = Field(...)
 
 
@@ -200,8 +200,35 @@ def home():
     tags=["Tweets"]
 )
 
-def post():
-    pass
+def post(tweet: Tweet = Body(...)):
+    """
+    Post a tweets
+    
+    This Path Operation post a tweet in the app
+
+    Parameters:
+        - Request Body Parameter
+            - user: Tweet
+
+    Returns a json with the basic tweet information:
+        - tweet_ID: UUID
+        - content: str
+        - created_at: datetime
+        - updated_at: datetime
+        - by: User
+    """
+    with open("tweets.json", "r+", encoding="utf-8") as f:
+        results = json.loads(f.read())
+        tweet_dict = tweet.dict()
+        tweet_dict["tweet_ID"] = str(tweet_dict["tweet_ID"])
+        tweet_dict["created_at"] = str(tweet_dict["created_at"])
+        tweet_dict["updated_at"] = str(tweet_dict["updated_at"])
+        tweet_dict["by"]["user_ID"] = str(tweet_dict["by"]["user_ID"])
+        tweet_dict["by"]["birth_date"] = str(tweet_dict["by"]["birth_date"])
+        results.append(tweet_dict)
+        f.seek(0)
+        f.write(json.dumps(results))
+        return tweet
 
 ### Show a tweet
 @app.get(
